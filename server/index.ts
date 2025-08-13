6e1480d1995fc8989e3da4783bdd2a6c58523282
malharshah2007-source <malharshah2007@gmail.com>
2025-08-13 10:33:25 +0000

    new changes

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,23 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  const port = parseInt(process.env.PORT || "5000", 10);
+  app._router.stack
+    .filter((r: any) => r.route && r.route.path)
+    .forEach((r: any) => {
+      const methods = Object.keys(r.route.methods)
+        .map((m) => m.toUpperCase())
+        .join(", ");
+      console.log(`${methods} ${r.route.path}`);
+    });
+  server.listen(
+    {
+      port,
+      host: "0.0.0.0",
+      reusePort: true,
+    },
+    () => {
+      log(`serving on port ${port}`);
+    },
+  );
 })();
